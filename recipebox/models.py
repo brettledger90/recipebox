f979d0316d9c1abe77815c78ce364e3413179e22
brett ledger <brett.ledger90@gmail.com>
2019-11-19 12:37:28 -0500

    Recipebox pt 1

---
+++ recipebox/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    biosection = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=50)
+    author= models.ForeignKey(Author, on_delete =models.CASCADE)
+    description = models.TextField()
+    instructions= models.TextField()
+    timerequired= models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title} - {self.author.name}"
